5a7f24735099263b6f5823ffa06efe2e045f59bd
nani0606 <87904703+nani0606@users.noreply.github.com>
2021-08-01 06:41:06 +0530

    Update hombotcode.py

--- hombotcode.py
+++ hombotcode.py
@@ -6,7 +6,7 @@ def lighton(bot,update):
   update.bot.sendPhoto(chat_id=chat_id,photo='https://static.scientificamerican.com/sciam/cache/file/2B38DE31-C1D3-4339-8808D61972976EE4.jpg')
   bot.message.reply_text("the light is on ")
   from Adafruit_IO import Client
-  aio = Client('anas1605', 'aio_oMzi613gjTj8LduHxpt8iDwWmMAq')
+  aio = Client('anas1605', 'aio_HkhB98E5BuknXMaxgCzfKy4ow3qU')
   aio.send('light', 1)
   data = aio.receive('light')
   print('Received value: {0}'.format(data.value))
@@ -17,7 +17,7 @@ def lightoff(bot,update):
   update.bot.sendPhoto(chat_id=chat_id,photo='https://ak.picdn.net/shutterstock/videos/16051507/thumb/1.jpg')
   bot.message.reply_text("the light is off")
   from Adafruit_IO import Client
-  aio = Client('anas1605', 'aio_oMzi613gjTj8LduHxpt8iDwWmMAq')
+  aio = Client('anas1605', 'aio_HkhB98E5BuknXMaxgCzfKy4ow3qU')
   aio.send('light', 0)
   data = aio.receive('light')
   print('Received value: {0}'.format(data.value))
@@ -28,7 +28,7 @@ def fanon(bot,update):
   update.bot.sendPhoto(chat_id=chat_id,photo="https://previews.123rf.com/images/llstock/llstock1909/llstock190900033/130060912-a-working-ceiling-fan-on-a-white-ceiling-close-up-with-blurred-fan-blades-three-lamps-daytime-.jpg")
   bot.message.reply_text("the fan is on")
   from Adafruit_IO import Client
-  aio = Client('anas1605', 'aio_oMzi613gjTj8LduHxpt8iDwWmMAq')
+  aio = Client('anas1605', 'aio_HkhB98E5BuknXMaxgCzfKy4ow3qU')
   aio.send('fan', 1)
   data = aio.receive('fan')
   print('Received value: {0}'.format(data.value))
@@ -39,7 +39,7 @@ def fanoff(bot,update):
   update.bot.sendPhoto(chat_id=chat_id,photo='https://images-na.ssl-images-amazon.com/images/I/31fec2V1l4L._SX425_.jpg')
   bot.message.reply_text("the fan is off")
   from Adafruit_IO import Client
-  aio = Client('anas1605', 'aio_oMzi613gjTj8LduHxpt8iDwWmMAq')
+  aio = Client('anas1605', 'aio_HkhB98E5BuknXMaxgCzfKy4ow3qU')
   aio.send('fan', 0)
   data = aio.receive('fan')
   print('Received value: {0}'.format(data.value))
